495a96b93b44a161d1e7d3810430324fbe4930b6
ayush-Ajmera <ayush.kholiya@ajmerainfotech.com>
2022-01-21 13:54:05 +0530

    add git ignore and e2e

---
+++ src/e2e/test/specs/example.e2e.js
@@ -0,0 +1,10 @@
+describe('React App - Home Screen', () => {
+    it('should have data of customer', async () => {
+
+        const SELECTED_CUSTOMER_TITLE = '[data-testid="SELECTED_CUSTOMER_TITLE"]';
+        const title = await $(SELECTED_CUSTOMER_TITLE);
+        await expect(title).toHaveText('John Smith');
+    });
+});
+
+
