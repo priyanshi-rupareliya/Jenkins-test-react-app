e241ab29af4f13e30a0c7c79707ba43b9f3415a3
ayushk048-ai <96562414+ayushk048-ai@users.noreply.github.com>
2022-01-27 12:58:52 +0530

    Update example.e2e.js

--- src/e2e/test/specs/example.e2e.js
+++ src/e2e/test/specs/example.e2e.js
@@ -3,7 +3,8 @@ describe('React App - Home Screen', () => {
 
         const SELECTED_CUSTOMER_TITLE = '[data-testid="SELECTED_CUSTOMER_TITLE"]';
         const title = await $(SELECTED_CUSTOMER_TITLE);
-        await expect(title).toHaveText('John Smith');
+//         await expect(title).toHaveText('John Smith');
+        await expect(title).toHaveText('Joh n Smith');
     });
 });
 
