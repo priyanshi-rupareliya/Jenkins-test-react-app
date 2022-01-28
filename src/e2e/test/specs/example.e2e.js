fe9ae08b845c0a7b6455d38bc4d6e839c65e398c
ayush-Ajmera <ayush.kholiya@ajmerainfotech.com>
2022-01-28 11:49:22 +0530

    Fixing typo

--- src/e2e/test/specs/example.e2e.js
+++ src/e2e/test/specs/example.e2e.js
@@ -1,12 +1,12 @@
 describe('Veggy - Home Screen', () => {
-    it('should have componet', async () => {
+    it('should have component', async () => {
 
         const PRODUCT_COMPONENT = '//*[@id="root"]/div/main/div/div/div[1]';
         const component = await $(PRODUCT_COMPONENT);
         expect(component).toBeExisting();
     });
 
-    it('should not have componet', async () => {
+    it('should not have component', async () => {
 
         const PRODUCT_COMPONENT = '//*[@id="root"]/div/main/div/div/div[1]';
         const component = await $(PRODUCT_COMPONENT);
