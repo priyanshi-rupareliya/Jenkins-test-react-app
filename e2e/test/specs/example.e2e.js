b8650dd944f8855a677828a5df7e89f0f8751a4a
Kamlesh Delat <kamlesh.delat@ajmerainfotech.com>
2022-01-29 16:23:17 +0530

    Write Mutiple Test Cases

--- e2e/test/specs/example.e2e.js
+++ e2e/test/specs/example.e2e.js
@@ -6,10 +6,42 @@ describe('Veggy - Home Screen', () => {
         expect(component).toBeExisting();
     });
 
-    it('should not have component', async () => {
-
-        const PRODUCT_COMPONENT = '//*[@id="root"]/div/main/div/div/div[1]';
-        const component = await $(PRODUCT_COMPONENT);
-        expect(component).toBeFalsy();
+    it('should have Brocolli Product', async () => {
+        await browser.pause(500);
+        const header = await $('h4*=Brocolli')
+        expect(header).toBeExisting();
     });
+
+
+    it('should have Cart info', async () => {
+        const header = await $('.cart-info')
+        expect(header).toBeExisting();
+    });
+
+    it('should have Search', async () => {
+        const header = await $('input[type="search"]')
+        expect(header).toBeExisting();
+    });
+
+    it('should have Product Price', async () => {
+        const header = await $('.product-price')
+        expect(header).toBeExisting();
+    });
+
+    it('should have Product Image', async () => {
+        const header = await $('.product-image')
+        expect(header).toBeExisting();
+    });
+
+    it('should not have alcohol Product', async () => {
+        const header = await $('h4*=alcohol');
+        expect(header).toBeFalsy();
+    });
+
+    // it('should not have component', async () => {
+
+    //     const PRODUCT_COMPONENT = '//*[@id="root"]/div/main/div/div/div[1]';
+    //     const component = await $(PRODUCT_COMPONENT);
+    //     expect(component).toBeFalsy();
+    // });
 });
